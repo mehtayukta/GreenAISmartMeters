efb4458aa56c24ab2c6879eeb4a500476b409c26
Yukta Mehta <69852469+mehtayukta@users.noreply.github.com>
2024-03-21 19:44:49 -0700

    Updated Code

--- FrontEnd/src/components/SidebarData.js
+++ FrontEnd/src/components/SidebarData.js
@@ -2,44 +2,100 @@ import React from 'react';
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
+import { GlobalContext } from './pages/GlobalContext';
+
 
 export const SidebarData = [
     {
       title: "Home",
       path: "/home",
       icon: <AiIcons.AiFillHome />,
-      cName: "nav-text"
+      cName: "nav-text",
+      handleClick: setGlobalVariable => setGlobalVariable("/home")
     },
     {
       title: "Device Management",
       path: "/device-mgmt",
       icon: <IoIcons.IoIosPaper />,
-      cName: "nav-text"
+      cName: "nav-text",
+      handleClick: setGlobalVariable => setGlobalVariable("/device-mgmt")
     },
     {
       title: "Monitor and Tracking",
       path: "/monitor-tracking-options",
       icon: <FaIcons.FaCartPlus />,
-      cName: "nav-text"
+      cName: "nav-text",
+      handleClick: setGlobalVariable => setGlobalVariable("/monitor-tracking-options")
     },
     {
       title: "Control & Configuration",
       path: "/control-config",
       icon: <IoIcons.IoMdPeople />,
-      cName: "nav-text"
+      cName: "nav-text",
+      handleClick: setGlobalVariable => setGlobalVariable("/control-configt")
     },
     {
       title: "Billing-Information",
       path: "/billing-info",
       icon: <FaIcons.FaFileInvoiceDollar/>,
-      cName: "nav-text"
+      cName: "nav-text",
+      handleClick: setGlobalVariable => setGlobalVariable("/billing-info")
     },
   
     {
       title: "Logout",
       path: "/",
       icon: <IoIcons.IoMdPeople />,
-      cName: "nav-text"
-    }
+      cName: "nav-text",
+      handleClick: setGlobalVariable => setGlobalVariable("/")
+    },
+
+  
   ];
+
+// import React from 'react';
+// import * as FaIcons from "react-icons/fa";
+// import * as AiIcons from "react-icons/ai";
+// import * as IoIcons from "react-icons/io";
+
+// export const SidebarData = [
+//     {
+//       title: "Home",
+//       path: "/home",
+//       icon: <AiIcons.AiFillHome />,
+//       cName: "nav-text"
+//     },
+//     {
+//       title: "Device Management",
+//       path: "/device-mgmt",
+//       icon: <IoIcons.IoIosPaper />,
+//       cName: "nav-text"
+//     },
+//     {
+//       title: "Monitor and Tracking",
+//       path: "/monitor-tracking-options",
+//       icon: <FaIcons.FaCartPlus />,
+//       cName: "nav-text"
+//     },
+//     {
+//       title: "Control & Configuration",
+//       path: "/control-config",
+//       icon: <IoIcons.IoMdPeople />,
+//       cName: "nav-text"
+//     },
+//     {
+//       title: "Billing-Information",
+//       path: "/billing-info",
+//       icon: <FaIcons.FaFileInvoiceDollar/>,
+//       cName: "nav-text"
+//     },
+  
+//     {
+//       title: "Logout",
+//       path: "/",
+//       icon: <IoIcons.IoMdPeople />,
+//       cName: "nav-text"
+//     }
+//   ];
+  
   
