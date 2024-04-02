dde469eda05448bc21b41c574c729d2c11fb64f6
Yukta Mehta <69852469+mehtayukta@users.noreply.github.com>
2024-04-02 13:45:12 -0700

    Updated 2nd Apr

--- FrontEnd/src/components/Admin/Admin.js
+++ FrontEnd/src/components/Admin/Admin.js
@@ -1,39 +1,70 @@
-import React from 'react'
-import '../../styles/device.css'
-import {AiFillHome} from 'react-icons/ai'
-import {GrUserSettings} from 'react-icons/gr'
-import {GoGraph} from 'react-icons/go'
-import {FaSignOutAlt} from 'react-icons/fa'
+import React, { useState, useContext } from 'react';
+import '../../styles/device.css';
+import { AiFillHome } from 'react-icons/ai';
+import { GrUserSettings } from 'react-icons/gr';
+import { GoGraph } from 'react-icons/go';
+import { FaSignOutAlt } from 'react-icons/fa';
 import { Link } from "react-router-dom";
-
+import { RiLeafFill } from 'react-icons/ri';
+import { GlobalContext } from '../pages/GlobalContext';
 
 export const SideBar = () => {
+  const { globalVariable, setGlobalVariable, globalVariable_2, setGlobalVariable_2 } = useContext(GlobalContext);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleClick = (destination) => {
+    setGlobalVariable(destination);
+    setSelectedItem(destination);
+  };
+
   return (
-    <>
     <div className='sidebar'>
-      <div >
-        <Link to="/adminhome"> <AiFillHome style={{ fontSize: '32px' }}/> </Link>
-      </div> <br/>
       <div>
-        <Link to="/adminmetrics"> <GoGraph style={{ fontSize: '32px' }}/> <br/> </Link>
-      </div> <br/>
+        <h3 style={{ fontSize: '20px', color: "white" }}> <RiLeafFill style={{ fontSize: '24px', color: "green" }} />Green Energy Cloud </h3>
+      </div>
+      <br />
       <div>
-        <Link to="/addclients"> <GrUserSettings style={{ fontSize: '31px' }}/> </Link>
+        <Link
+          to="/home"
+          style={{ fontSize: '20px', color: "rgb(117, 117, 117)" }}
+          className={selectedItem === "/home" ? "sidebar-item selected" : "sidebar-item"}
+          onClick={() => handleClick("/home")}
+        >
+          <AiFillHome style={{ fontSize: '20px', color: "rgb(117, 117, 117)" }} /> Home
+        </Link>
       </div>
-      <div> <br/>
-        <Link to= "/"> <FaSignOutAlt style={{ fontSize: '31px' }}/> </Link>
+      <br />
+      <div>
+        <Link
+          to="/device-mgmt"
+          style={{ fontSize: '20px', color: "rgb(117, 117, 117)" }}
+          className={selectedItem === "/device-mgmt" ? "sidebar-item selected" : "sidebar-item"}
+          onClick={() => handleClick("/device-mgmt")}
+        >
+          <GoGraph style={{ fontSize: '20px', color: "rgb(117, 117, 117)" }} /> Device Management
+        </Link>
       </div>
+      <br />
+      <div>
+        <Link
+          to="/monitor-tracking-options"
+          style={{ fontSize: '20px', color: "rgb(117, 117, 117)" }}
+          className={selectedItem === "/monitor-tracking-options" ? "sidebar-item selected" : "sidebar-item"}
+          onClick={() => handleClick("/monitor-tracking-options")}
+        >
+          <GrUserSettings style={{ fontSize: '20px', color: "rgb(117, 117, 117)" }} /> Monitoring-Tracking
+        </Link>
       </div>
-    </>
+      <div> <br />
+        <Link to="/" style={{ fontSize: '20px', color: "rgb(117, 117, 117)" }}> <FaSignOutAlt style={{ fontSize: 'rgb(117, 117, 117)' }} /> Logout </Link>
+      </div>
+    </div>
   );
 };
 
-
 export const Admin = () => {
   return (
-      <div style={{display: 'flex'}}>
-        
-      </div>
+    <div style={{ display: 'flex' }}></div>
   );
-}
+};
 
